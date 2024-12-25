263061ca48ec9ce7e3f6e11de85be05310777375
nikunja kathrotiya <patelnikunja357@gmail.com>
2024-12-25 17:46:48 +0530

    [NK]

---
+++ variable.js
@@ -0,0 +1,22 @@
+let text = "hello world";
+let firstName = "jonas";
+const PI = 333.55;
+let lastName = "welcome";
+let isJsFun = true;
+let fruits = "grapes";
+let year = 2000;
+const ageJonas = 2025 - 2004;
+
+` text : ${text}`
+console.log(`
+    Text: ${text}
+    First: ${firstName}
+    PI: ${PI}
+    Last: ${lastName}
+    isJsFun: ${typeof isJsFun} 
+    isJsFun : ${typeof "techrover"} 
+    Type of null: ${typeof null}
+    Fruits: ${fruits}
+    Year: ${year}
+    Age Difference: ${ageJonas}
+`);
